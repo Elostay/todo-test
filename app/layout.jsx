4683a93495610ed1c1b9b03593a5887ee856c111
Elostay <nastia60203@gmail.com>
2024-01-23 14:02:09 +0200

    set up the project

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Todo-list",
+  description: "Track your tasks",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
